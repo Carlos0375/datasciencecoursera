621435feaf74203a8f81ca5c03dc0dee7a8a2c06
carlhuelgas <carl.huelgas@gmx.de>
2015-09-12 17:25:49 +0200

    Verison 12.09.15 17:25

--- R programming.R
+++ R programming.R
@@ -1,4 +1,7 @@
 # Programming in R - Thoughts and Examples
+# This are just small scripts for creating simple functions. This way I try to learn R. The functions my not be
+# perfect but they should work. With every new function I create I try to implement something new and more elegant...
+
 
 # Used or practical packages...
 ############################################################################################
@@ -56,4 +59,29 @@ f_vector_two <- function(n,m){
   return(list(v1=vector01,v2=vector02))
 } 
 
-f_vector_two(10,3)
+f_vector_two(10,3.75)
+
+# create a data.frame out of two vectors
+
+f_vector_df <- function(n,m){
+  # error-handling
+  if(mode(n) != "numeric")
+    stop("need numeric data")
+  else
+    # create an empty vector 1 und 2
+    vector01 <- vector("list",n)
+  vector02 <- vector("list",n)
+  # for-construction
+  for(i in 1:n){
+    # a values 1 to i to the vector
+    vector01[i] <- i
+    vector02[i] <- i*m
+  }
+  # vector01 <- unlist(vector01)
+  # vector02 <- unlist(vector02)
+  mydf01 <- as.data.frame(rbind(c(vector01,vector02)))
+  # print vecotr01 and vector02
+  return(mydf01)
+} 
+
+f_vector_df(10,2)
