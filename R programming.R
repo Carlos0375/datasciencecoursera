ce0ec5b3aa32fe8de7da91ae613e4b3b11d5161d
carlhuelgas <carl.huelgas@gmx.de>
2015-09-12 17:08:21 +0200

    Version 12.09.15 17:00

--- R programming.R
+++ R programming.R
@@ -34,4 +34,26 @@ f_vector <- function(n){
 
 f_vector(10)
 
+# create two vectors
 
+f_vector_two <- function(n,m){
+  # error-handling
+  if(mode(n) != "numeric")
+    stop("need numeric data")
+  else
+    # create an empty vector 1 und 2
+    vector01 <- vector("list",n)
+    vector02 <- vector("list",n)
+  # for-construction
+  for(i in 1:n){
+    # a values 1 to i to the vector
+    vector01[i] <- i
+    vector02[i] <- i*m
+  }
+  vector01 <- unlist(vector01)
+  vector02 <- unlist(vector02)
+  # print vecotr01 and vector02
+  return(list(v1=vector01,v2=vector02))
+} 
+
+f_vector_two(10,3)
